9a3af83f7a0a70e85d6af9deca144f4517d9ed42
gommness <gommness@gmail.com>
2019-03-07 22:42:49 +0100

    started working in the glosary + CompositeCondition lil progress

--- conditionSyntax/compositecondition.cpp
+++ conditionSyntax/compositecondition.cpp
@@ -3,6 +3,7 @@
 CompositeCondition::CompositeCondition(const QString &input)
 {
     QStringList list = CompositeCondition::customSplit(input);
+    conditions = parseConditions(list);
     // first condition in the QConditionsList will have the empty connector. the rest would have the corresponding connector
     // keep in mind that a Condition can be either a CompositeCondition or a SimpleCondition.
 }
@@ -27,9 +28,43 @@ QString CompositeCondition::toString()
 
 }
 
-CompositeCondition::Conector CompositeCondition::parseConnector(const QString &conector)
+CompositeCondition::Connector CompositeCondition::parseConnector(const QString &conector)
 {
 
+}
+
+CompositeCondition::QConditionsList CompositeCondition::parseConditions(const QStringList &list){
+    QConditionsList output; // list of Connector-Condition.
+    Connector *connector;
+    Condition *condition;
+    QPair<Connector, Condition> pair = QPair<Connector, Condition>();
+
+    connector = new Connector();
+    *connector = Connector::EMPTY;
+    condition = new Condition();
+
+    for(int i = 0; i < list.length(); i++){ // iterate throught the list of "words"
+        if(list[i] == "("){ // if it is a condition between parenthesis, then it is composite.
+            // create a composite condition
+        } else if(list[i] == ")"){ // if it is the end of a composite condition
+            // we'd end the creation of this composite condition
+            // then return it. BUT ALSO, we should somehow throw outside the
+            // index of the QStringList that we are at right now !!!!
+            // TODO do this last part
+        } else {
+            // create SimpleCondition
+        }
+        // TODO create the condition, either simple condition or composite condition
+        //      then advance with the list until a connector is reached
+
+        pair.first = *connector;
+        pair.second = *condition;
+        output.append(pair);
+
+        *connector = parseConnector(list[i]);
+    }
+    delete connector;
+    return output;
 }
 
 QStringList CompositeCondition::customSplit(const QString &input)
@@ -38,7 +73,7 @@ QStringList CompositeCondition::customSplit(const QString &input)
     QStringList splittedList = input.split(QRegExp("[ ]"), QString::SkipEmptyParts);
     foreach( QString word, splittedList){
         QString cleansedWord(word);
-        cleansedWord.remove(QRegExp("[())]")); // cleansed word would be the word but removing all "(" and ")" in it if they existed
+        cleansedWord.remove(QRegExp("[)()]")); // cleansed word would be the word but removing all "(" and ")" in it if they existed
 
         if(word.front() == "(") // if the word started with (, add it as a string to the output
             output.append("(");
